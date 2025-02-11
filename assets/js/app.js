971dd5633261146fdaeb699748e14292dadfc9bc
mistertlatoani <tlatoani.green@Outlook.com>
2025-02-10 21:15:02 -0600

    feat: CorreccionDeCodigos1

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
+
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
+
 const $n = document.querySelector('name');
 const $b = document.querySelector('#blog');
 const $l = document.querySelector('.location');
@@ -7,15 +9,28 @@ const $l = document.querySelector('.location');
 async function displayUser(username) {
   $n.textContent = 'cargando...';
   const response = await fetch(`${usersEndpoint}/${username}`);
+  //Agrgere una verificacion si la respuesta es exitosa.
+  if (!response.ok) {
+    throw new Error( `Usuario no encontrado: ${response.statusText}`);
+
+    //pasar la respuesta a Json
+const data = await response.json();
+
+  } 
   console.log(data);
   $n.textContent = '${data.name}';
   $b.textContent = '${data.blog}';
   $l.textContent = '${data.location}';
-};
+} catch (err) {
 
+// manejar errores y mostrar en mensaje eb el dom
+  handleError(err);
+  }
+}
+
+// le quite un mensaje de error por que ya tenia uno, el otro pienso essbaba de mas
 function handleError(err) {
   console.log('OH NO!');
-  console.log(err);
   n.textContent = `Algo salió mal: ${err}`
 };
 
